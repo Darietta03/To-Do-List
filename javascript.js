ab1060e6ed680424da0f9badde497bbfba6f146c
Darietta03 <dsnegireva2017@gmail.com>
2023-01-28 22:28:57 +0200

    changes28/01/23

--- javascript.js
+++ javascript.js
@@ -2,23 +2,33 @@
 let newtask = document.querySelector('.createTask');
 let addButton = document.querySelector('.addButton');
 let tasks = document.querySelector('.addedTasks');
+let error = document.querySelector('#errorModal');
+let okButton = document.querySelector(".agree");
 
-addButton.addEventListener('click', addToList);
 
-function addToList() {
-    if(newtask.value.length < 1) {
-        
+
+
+addButton.addEventListener('click', () => {
+    if(!newtask.value.length) {
+        error.classList.add("errorOpen");
+       
     } else {
         tasks.innerHTML +=
         `<div class="task">
-            <span id=""taskname>${newtask.value}</span>
+            <span id="taskname">${newtask.value}</span>
             <button class="deleteTask">
-            <span class="material-symbols-outlined">
-delete
-</span>
+                <span class="material-symbols-outlined"> delete </span>
             </button>
         </div>
         <hr>`
-    }
 
-}
+        newtask.value = "";
+    }
+});
+
+okButton.addEventListener('click', () => {
+    error.classList.remove("errorOpen");
+  }); 
+
+
+
