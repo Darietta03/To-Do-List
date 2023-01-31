810ed6636313292dfcff1c38635b35829b2d9914
Darietta03 <dsnegireva2017@gmail.com>
2023-01-31 21:39:20 +0200

    To Do List

--- javascript.js
+++ javascript.js
@@ -6,29 +6,55 @@ let error = document.querySelector('#errorModal');
 let okButton = document.querySelector(".agree");
 
 
-
-
 addButton.addEventListener('click', () => {
+
     if(!newtask.value.length) {
-        error.classList.add("errorOpen");
-       
+        error.classList.add("errorOpen");  
     } else {
         tasks.innerHTML +=
         `<div class="task">
             <span id="taskname">${newtask.value}</span>
             <button class="deleteTask">
                 <span class="material-symbols-outlined"> delete </span>
-            </button>
+            </button>  
         </div>
-        <hr>`
-
-        newtask.value = "";
+       
+        `  
     }
+    newtask.value = "";
 });
 
 okButton.addEventListener('click', () => {
     error.classList.remove("errorOpen");
   }); 
+
+
+tasks.addEventListener('click', function(event) {
+
+    let crossTask = event.target.closest('#taskname');
+    if(crossTask) {
+        crossTask.classList.toggle("lineThrought");
+    }  
+});
+
+
+
+tasks.addEventListener('click', function(event) {
+    let deleteTask = event.target.closest('.task');
+    let deleteBtn = event.target.closest('.deleteTask');
+   
+    if(deleteBtn) {
+       deleteTask.remove();
+    }  
+});
+
+
+
+
+
+
+
+
 
 
 
